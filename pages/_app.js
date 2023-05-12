fa874604117809465f5cbd39d41beea7b5efdcec
ThomasF85 <thomas.foeldi@gmail.com>
2023-05-12 14:05:23 +0200

    remove custom local storage hook impl

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import GlobalStyle from "../styles";
 import useSWR from "swr";
 import Layout from "../components/Layout.js";
-import { useImmerLocalStorageState } from "../lib/hook/useImmerLocalStorageState.js";
+import useLocalStorageState from "use-local-storage-state";
 
 const fetcher = async (...args) => {
   const response = await fetch(...args);
@@ -16,31 +16,31 @@ export default function App({ Component, pageProps }) {
     "https://example-apis.vercel.app/api/art",
     fetcher
   );
-  const [artPiecesInfo, setArtPiecesInfo] = useImmerLocalStorageState(
+  const [artPiecesInfo, setArtPiecesInfo] = useLocalStorageState(
     "art-pieces-info",
     { defaultValue: [] }
   );
 
   function handleToggleFavorite(slug) {
-    const artPiece = artPiecesInfo.find((piece) => piece.slug === slug);
-    if (artPiece) {
-      setArtPiecesInfo(
-        artPiecesInfo.map((pieceInfo) =>
+    setArtPiecesInfo((prev) => {
+      const artPiece = prev.find((piece) => piece.slug === slug);
+      if (artPiece) {
+        return prev.map((pieceInfo) =>
           pieceInfo.slug === slug
             ? { ...pieceInfo, isFavorite: !pieceInfo.isFavorite }
             : pieceInfo
-        )
-      );
-    } else {
-      setArtPiecesInfo([...artPiecesInfo, { slug, isFavorite: true }]);
-    }
+        );
+      } else {
+        return [...prev, { slug, isFavorite: true }];
+      }
+    });
   }
 
   function addComment(slug, newComment) {
-    const artPiece = artPiecesInfo.find((piece) => piece.slug === slug);
-    if (artPiece) {
-      setArtPiecesInfo(
-        artPiecesInfo.map((pieceInfo) => {
+    setArtPiecesInfo((prev) => {
+      const artPiece = prev.find((piece) => piece.slug === slug);
+      if (artPiece) {
+        return prev.map((pieceInfo) => {
           if (pieceInfo.slug === slug) {
             return pieceInfo.comments
               ? { ...pieceInfo, comments: [...pieceInfo.comments, newComment] }
@@ -48,14 +48,11 @@ export default function App({ Component, pageProps }) {
           } else {
             return pieceInfo;
           }
-        })
-      );
-    } else {
-      setArtPiecesInfo([
-        ...artPiecesInfo,
-        { slug, isFavorite: false, comments: [newComment] },
-      ]);
-    }
+        });
+      } else {
+        return [...prev, { slug, isFavorite: false, comments: [newComment] }];
+      }
+    });
   }
 
   return (
