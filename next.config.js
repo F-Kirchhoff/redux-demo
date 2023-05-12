0577f92bf5fb1f6ec43a12e68ceed923932488e4
ThomasF85 <thomas.foeldi@gmail.com>
2023-05-12 09:52:50 +0200

    implement full app with local state

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
     styledComponents: true,
   },
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example-apis.vercel.app",
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
-export default function HomePage() {
+import Head from "next/head.js";
+import Spotlight from "../components/Spotlight/index.js";
+
+export default function SpotlightPage({
+  pieces,
+  artPiecesInfo,
+  onToggleFavorite,
+}) {
+  const spotlightPiece =
+    pieces[Math.floor(Math.random() * (pieces.length - 1))];
+
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <>
+      <Head>
+        <title>Art Gallery - Spotlight</title>
+      </Head>
+      {spotlightPiece && (
+        <Spotlight
+          image={spotlightPiece.imageSource}
+          artist={spotlightPiece.artist}
+          isFavorite={
+            artPiecesInfo.find((piece) => piece.slug === spotlightPiece.slug)
+              ?.isFavorite
+          }
+          onToggleFavorite={() => onToggleFavorite(spotlightPiece.slug)}
+        />
+      )}
+    </>
   );
 }
