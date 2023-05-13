70add3f2d345b579bf2cd1c5fa83b16e32a3c627
ThomasF85 <thomas.foeldi@gmail.com>
2023-05-13 09:30:47 +0200

    add shuffle button to spotlight page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,45 @@
 import Head from "next/head.js";
 import Spotlight from "../components/Spotlight/index.js";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const ButtonContainer = styled.div`
+  text-align: center;
+`;
+
+const Button = styled.button`
+  background-color: lightcoral;
+  border: 3px solid black;
+  border-radius: 0.5rem;
+  padding: 0.5rem;
+  font-weight: bold;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.7;
+  }
+`;
+
+function randomIndex(arrayLength) {
+  return Math.floor(Math.random() * (arrayLength - 1));
+}
 
 export default function SpotlightPage({
   pieces,
   artPiecesInfo,
   onToggleFavorite,
 }) {
-  const spotlightPiece =
-    pieces[Math.floor(Math.random() * (pieces.length - 1))];
+  const [index, setIndex] = useState(null);
+
+  useEffect(() => {
+    if (pieces) {
+      setIndex(randomIndex(pieces.length));
+    } else {
+      setIndex(null);
+    }
+  }, [pieces]);
+
+  const spotlightPiece = index === null ? null : pieces[index];
 
   return (
     <>
@@ -15,15 +47,25 @@ export default function SpotlightPage({
         <title>Art Gallery - Spotlight</title>
       </Head>
       {spotlightPiece && (
-        <Spotlight
-          image={spotlightPiece.imageSource}
-          artist={spotlightPiece.artist}
-          isFavorite={
-            artPiecesInfo.find((piece) => piece.slug === spotlightPiece.slug)
-              ?.isFavorite
-          }
-          onToggleFavorite={() => onToggleFavorite(spotlightPiece.slug)}
-        />
+        <>
+          <Spotlight
+            image={spotlightPiece.imageSource}
+            artist={spotlightPiece.artist}
+            isFavorite={
+              artPiecesInfo.find((piece) => piece.slug === spotlightPiece.slug)
+                ?.isFavorite
+            }
+            onToggleFavorite={() => onToggleFavorite(spotlightPiece.slug)}
+          />
+          <ButtonContainer>
+            <Button
+              type="button"
+              onClick={() => setIndex(randomIndex(pieces.length))}
+            >
+              shuffle
+            </Button>
+          </ButtonContainer>
+        </>
       )}
     </>
   );
